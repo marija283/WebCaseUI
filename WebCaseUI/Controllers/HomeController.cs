f3b1c67ba0b446fbe3e06351d4d5bb7d08c484df
Marija Todorova <marija283@hotmail.com>
2017-08-30 15:14:37 +0200

    post case

--- WebCaseUI/Controllers/HomeController.cs
+++ WebCaseUI/Controllers/HomeController.cs
@@ -36,6 +36,14 @@ namespace WebCaseUI.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Case(Case myCase)
+        {
+            ViewBag.Message = "Your case page.";
+            var myCaseReponse = await createCaseAsync(myCase);
+            return View();
+        }
+
         public async Task<ActionResult> All()
         {
             var allCase = await GetAllCaseAsync();
@@ -92,7 +100,20 @@ namespace WebCaseUI.Controllers
             }
         }
 
+        static async Task<Case> createCaseAsync(Case myCase)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8787/");
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/cases", myCase);
+                response.EnsureSuccessStatusCode();
 
+            }
+
+            // Deserialize the updated product from the response body.
+             //  Case myCase = await response.Content.ReadAsAsync<Case>();
+            return myCase;
+        }
 
         static async Task<Case> UpdateProductAsync(int myCaseId, HttpPostedFileBase file)
         {
