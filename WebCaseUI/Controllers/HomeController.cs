f15393a50ab9a3943081ea7a28292f94d5675263
Marija Todorova <marija283@hotmail.com>
2017-08-30 13:05:48 +0200

    update

--- WebCaseUI/Controllers/HomeController.cs
+++ WebCaseUI/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebCaseUI.Models;
 
 namespace WebCaseUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        static HttpClient client = new HttpClient();
+        public HomeController()
+        {
+            client.BaseAddress = new Uri("http://localhost:8787/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -31,6 +44,29 @@ namespace WebCaseUI.Controllers
             ViewBag.Message = "Your update page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, HttpPostedFileBase uploadFile)
+        {
+            ViewBag.Message = "Your update post page.";
+
+            return View();
+        }
+
+
+
+
+
+        static async Task<Case> UpdateProductAsync(Case myCase, HttpPostedFileBase uploadFile)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/cases/{myCase.ID}", uploadFile);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            myCase = await response.Content.ReadAsAsync<Case>();
+            return myCase;
         }
     }
 }
---
+++ WebCaseUI/Models/Case.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCaseUI.Models
+{
+    public class Case
+    {
+        public int ID { get; set; }
+
+        public String caseNumber { get; set; }
+
+        public String kind { get; set; }
+
+        public String customerNumber { get; set; }
+
+        public String attachment { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Case Number: {0}, Kind:{1}", caseNumber, kind);
+        }
+
+
+        // public String status { get; set; }
+
+        //  public String resolution { get; set; }
+
+        //   public String importance { get; set; }
+
+        //   public String title { get; set; }
+
+        //   public String details { get; set; }
+
+        //   public String arrangementNumber { get; set; }
+    }
+}
